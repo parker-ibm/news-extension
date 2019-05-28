395fba37a46cf75179ee2a0494e44a805891c161
Parker Merritt <parkeribm@parkers-mbp.atlanta.ibm.com>
2019-05-28 11:24:37 -0400

    Enable background change on Facebook

--- background.js
+++ background.js
@@ -4,9 +4,13 @@ chrome.runtime.onInstalled.addListener(function() {
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
+        conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { urlContains: '.developer.chrome.com' },
+            }),
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostContains: '.facebook.com' },
+            })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
