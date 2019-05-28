e15e86aaa71554ec424c9939562587cd4e84e4d4
Parker Merritt <parkeribm@parkers-mbp.atlanta.ibm.com>
2019-05-28 11:07:42 -0400

    Add color change option panel

--- background.js
+++ background.js
@@ -3,12 +3,12 @@ chrome.runtime.onInstalled.addListener(function() {
       console.log("The color is green.");
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
   });
